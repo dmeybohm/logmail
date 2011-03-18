4b5dd2932fb9a3acf3c8e7576dfcf8e583022e9e
David Meybohm <dmeybohm@gmail.com>
2011-03-18 10:24:28 -0400

    call htmlentities on headers

--- message-template.php
+++ message-template.php
@@ -3,7 +3,7 @@ $message = new Zend_Mail_Message(array('raw' => $email->message));
 //print_r(get_class_methods($message));
 $headers = $message->getHeaders();
 foreach ($headers as $key => $value) {
-	echo ucfirst($key), ": ", $value, "<br />";
+	echo ucfirst($key), ": ", htmlentities($value), "<br />";
 }
 echo "<br />";
 //if (strpos($headers['content-type'], 'text/html') !== false) {
