0c1119597949695298441129f26a750bbdbaa25a
David Meybohm <dmeybohm@gmail.com>
2012-01-31 17:26:39 -0500

    Format

--- message-template.php
+++ message-template.php
@@ -1,7 +1,8 @@
 <?php
+
 $message = new Zend_Mail_Message(array('raw' => $email->message));
-//print_r(get_class_methods($message));
 $headers = $message->getHeaders();
+
 foreach ($headers as $key => $value) {
 	echo ucfirst($key), ": ", htmlentities($value), "<br />";
 }
